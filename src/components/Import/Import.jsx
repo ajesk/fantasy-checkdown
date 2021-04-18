7a75a30f3d8b899e5bf3310a9d61690cf3b0a02e
Aaron Jeskie <aaronjeskie@gmail.com>
2021-04-18 10:09:07 -0400

    feat: updated the import components

--- src/components/Import/Import.jsx
+++ src/components/Import/Import.jsx
@@ -1,29 +1,80 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 import { importPlayers } from '../../actions';
-import { parseFpCsv, importCsvData } from '../../util/parseRankings';
-import CSVReader from 'react-csv-reader';
-import Button from '@material-ui/core/Button';
+import { parseFpCsv } from '../../util/parseRankings';
+import { Button, Input, ButtonBase, Grid, TextField, Typography, Divider } from '@material-ui/core';
 import ImportExportIcon from '@material-ui/icons/ImportExport';
+import SaveIcon from '@material-ui/icons/Save';
 
-import './Import.scss';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  header: {
+    paddingBottom: '2em'
+    
+  },
+  importBox: {
+    paddingBottom: '2em'
+  },
+  fileUploadButton: {
+    marginRight: '1em'
+  },
+  csvTextBox: {
+    width: '100%'
+  }
+});
 
 const Import = ({ dispatch }) => {
+  const classes = useStyles();
   const [rawData, setData] = useState('');
+  const [fileName, setFileName] = useState('');
 
   const handleTAChange = (e) => setData(e.target.value);
 
   const handleSubmit = () => dispatch(importPlayers(parseFpCsv(rawData)));
 
-  const handleCsvImport = (csv) => dispatch(importPlayers(importCsvData(csv)));
+  const handleTest = (e) => {
+    const [file] = e.target.files;
+    setFileName(file.name);
+    file.text().then(t => setData(t));
+  };
 
   return (
-    <div className="import-control">
-      <FormGroup controlId="formControlsTextarea">
-        <ControlLabel>Paste Tier Rankings</ControlLabel>
-        <FormControl onChange={handleTAChange} componentClass="textarea" placeholder="tier rankings..." />
-      </FormGroup>
+    <Grid container direction="column" alignContent="center" justifyContent="center">
+      <Grid item className={classes.header}>
+        <Typography variant="h2">Import FP CSV Data</Typography>
+      </Grid>
+      <Grid container direction="row" alignContent="center" justify="space-evenly" className={classes.importBox}>
+        <Grid container item xs={4} alignContent="center" justify="center">
+          <TextField
+            id="outlined-multiline-static"
+            label="Paste Ranking CSV Text"
+            multiline
+            rows={4}
+            variant="outlined"
+            onChange={handleTAChange}
+            className={classes.csvTextBox}
+          />
+        </Grid>
+        <Divider orientation="vertical" flexItem />
+        <Grid container item xs={4} alignContent="center" justify="center">
+          <ButtonBase
+            component="label"
+          >
+            <Button variant="contained" startIcon={<SaveIcon />} component="span" className={classes.fileUploadButton}>
+              Upload
+          </Button>
+            <Typography>{fileName}</Typography>
+            <Input
+              style={{ display: 'none' }}
+              accept=".csv"
+              id="contained-button-file"
+              multiple
+              onChange={handleTest}
+              type="file" />
+          </ButtonBase>
+        </Grid>
+      </Grid>
       <Button
         variant="contained"
         color="primary"
@@ -32,15 +83,7 @@ const Import = ({ dispatch }) => {
       >
         Import
       </Button>
-      <CSVReader
-        cssClass="csv-reader-input"
-        label="Import FP CSV Rankings"
-        onFileLoaded={handleCsvImport}
-        // onError={handleDarkSideForce}
-        inputId="fp-rankings"
-        inputStyle={{ color: 'red' }}
-      />
-    </div>
+    </Grid>
   );
 };
 
