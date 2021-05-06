39bb554762fb6c2f710cf9b649d137d00718e047
Aaron Jeskie <aaronjeskie@gmail.com>
2021-05-05 20:15:56 -0400

    Updates

---
+++ precache-manifest.07688605c46bfe2e1bd34523f4312980.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9317ebe73a954c4e13de24ef565b51b",
+    "url": "/fantasy-checkdown/index.html"
+  },
+  {
+    "revision": "7c05048e93923ecdd0db",
+    "url": "/fantasy-checkdown/static/css/main.7ca993ba.chunk.css"
+  },
+  {
+    "revision": "9c83d28e8b8b6fc74d25",
+    "url": "/fantasy-checkdown/static/js/2.8ec5d5aa.chunk.js"
+  },
+  {
+    "revision": "385801f39fe2615b5790da562b481c6c",
+    "url": "/fantasy-checkdown/static/js/2.8ec5d5aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c05048e93923ecdd0db",
+    "url": "/fantasy-checkdown/static/js/main.972b8d7b.chunk.js"
+  },
+  {
+    "revision": "9f8a6b5e376600409b77",
+    "url": "/fantasy-checkdown/static/js/runtime-main.93c499ae.js"
+  }
+]);
