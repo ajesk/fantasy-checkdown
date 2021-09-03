4bcb75db37a7ffcacb05a6216e8548acb60ececc
Aaron Jeskie <aaronjeskie@gmail.com>
2021-09-02 21:29:22 -0400

    fixing a name

--- src/components/Content/Reset/Reset.test.jsx
+++ src/components/Content/Reset/Reset.test.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import Undo from './Reset';
+import Reset from './Reset';
 import { render, fireEvent } from '@testing-library/react'
 import { createStore } from 'redux';
 import rootReducer from '../../../reducers';
 
 it('renders with no button', () => {
-  const { queryByText } = render(<Undo store={createStore(rootReducer)} players={[]} />);
+  const { queryByText } = render(<Reset store={createStore(rootReducer)} players={[]} />);
 
-  expect(queryByText("Undo")).toBeNull();
+  expect(queryByText("Reset")).toBeNull();
 });
 
 it('renders with button', () => {
   const store = createStore(rootReducer, { players: [{ name: "Player Name" }] });
-  const { getByText } = render(<Undo store={store} />);
+  const { getByText } = render(<Reset store={store} />);
 
-  expect(getByText("Undo")).toBeTruthy();
+  expect(getByText("Reset")).toBeTruthy();
 });
 
 it('should fire undo dispatch', () => {
@@ -24,7 +24,7 @@ it('should fire undo dispatch', () => {
   const store = createStore(rootReducer, { players: [{ name: "Player Name" }] });
   store.dispatch = mockDispatch;
 
-  const { getByText } = render(<Undo store={store} />);
-  fireEvent.click(getByText("Undo"))
+  const { getByText } = render(<Reset store={store} />);
+  fireEvent.click(getByText("Reset"))
   expect(counter).toBe(1);
 });
